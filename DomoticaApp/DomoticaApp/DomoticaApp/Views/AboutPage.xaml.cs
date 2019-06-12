fd16f4f37efc9adf3db9a46062e9a50a2121cabb
Damiaen <dtoussaint97@gmail.com>
2019-06-12 11:28:00 +0200

    Added new project
    Moved old project and added new

---
+++ DomoticaApp/DomoticaApp/DomoticaApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DomoticaApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
