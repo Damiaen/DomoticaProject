1f76600228e212cdf75ffb9edd288e9f4d3967c9
Damiaen <dtoussaint97@gmail.com>
2019-06-24 13:31:46 +0200

    Added new pages

---
+++ Domotica/Domotica/Domotica/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Domotica.Models
+{
+    public class Profile
+    {
+        [PrimaryKey, AutoIncrement, Unique, NotNull]
+        public int Profile_id { get; set; }
+        public int RFID_id { get; set; }
+        public string AnimalName { get; set; }
+        public string PortionSize { get; set; }
+    }
+}
